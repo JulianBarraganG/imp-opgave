e4fcc2e10b3c28db47fc6bda6dfea749e39bd1dc
OskarLinke <olinke2@gmail.com>
2025-05-21 10:51:37 +0200

    Created the project structure

---
+++ src/Program.cs
@@ -0,0 +1,11 @@
+﻿
+    public class Program
+{
+    static public void Main(string[] args)
+    {
+        int result = BitOperations.RightShift(8, 2);
+        Console.WriteLine($"The result of right shifting 8 by 2 is: {result}");
+    
+
+    }
+}
